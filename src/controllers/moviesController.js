5d0cc0b130ec594ef81c029495e22321297888f5
hernan0066 <hernan0066@gmail.com>
2022-09-16 18:22:15 -0300

    Initial commit

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,73 @@
+const db = require("../database/models");
+const sequelize = db.sequelize;
+
+//Otra forma de llamar a los modelos
+const Movies = db.Movie;
+
+const moviesController = {
+  list: (req, res) => {
+    db.Movie.findAll().then((movies) => {
+      res.render("moviesList.ejs", { movies });
+    });
+  },
+  detail: (req, res) => {
+    db.Movie.findByPk(req.params.id).then((movie) => {
+      res.render("moviesDetail.ejs", { movie });
+    });
+  },
+  new: (req, res) => {
+    db.Movie.findAll({
+      order: [["release_date", "DESC"]],
+      limit: 5,
+    }).then((movies) => {
+      res.render("newestMovies", { movies });
+    });
+  },
+  recomended: (req, res) => {
+    db.Movie.findAll({
+      where: {
+        rating: { [db.Sequelize.Op.gte]: 8 },
+      },
+      order: [["rating", "DESC"]],
+    }).then((movies) => {
+      res.render("recommendedMovies.ejs", { movies });
+    });
+  }, //Aqui debemos modificar y completar lo necesario para trabajar con el CRUD
+  add: function (req, res) {
+    db.Genre.findAll({
+      order: ["name"],
+    })
+      .then((genres) => {
+        res.render("moviesAdd", { genres });
+      })
+      .catch((err) => console.log(err));
+  },
+  create: function (req, res) {
+    db.Movie.create({
+      title: req.body.title,
+      rating: req.body.rating,
+      length: req.body.length,
+      awards: req.body.awards,
+      release_date: req.body.release_date,
+      genre_id: req.body.genre,
+    })
+      .then((movie) => {
+        res.redirect('/movies')
+      })
+      .catch((error) => console.log(error));
+  },
+  edit: function (req, res) {
+    // TODO
+  },
+  update: function (req, res) {
+    // TODO
+  },
+  delete: function (req, res) {
+    // TODO
+  },
+  destroy: function (req, res) {
+    // TODO
+  },
+};
+
+module.exports = moviesController;
